769e59d51c2afea9aef78451d59bddc02d499088
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-03-23 11:16:24 +0100

    Let controller save to repository

--- src/main/java/se/codelabs/timereport/api/controller/EventController.java
+++ src/main/java/se/codelabs/timereport/api/controller/EventController.java
@@ -1,29 +1,24 @@
 package se.codelabs.timereport.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import se.codelabs.timereport.api.model.Event;
-import se.codelabs.timereport.api.model.EventId;
 import se.codelabs.timereport.api.repositories.EventRepository;
 
-import java.util.List;
 
 @RestController
 public class EventController {
 
+    @Autowired
     private final EventRepository repository;
 
     EventController(EventRepository repository) {
         this.repository = repository;
     }
 
-    @GetMapping("/events")
-    public String eventRoot() {
-        return "{\"name\": \"events api\", \"version\": 0.1}";
-    }
-
     @PostMapping(path = "/events", consumes = "application/json", produces = "application/json")
-    Event newEvent(@RequestBody Event newEvent) {
-        return repository.save(newEvent);
+    Event event(@RequestBody Event event) {
+        return repository.save(event);
     }
 
 }
