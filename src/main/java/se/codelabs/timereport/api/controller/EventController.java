3c4be225814f9a8401d65ccfe65ae8bfb85abc9b
Tommy Malmqvist <tommy.malqmvist@codelabs.se>
2019-03-23 23:59:03 +0100

    Add getEventByUserId and getEventByUserIdAndDate

--- src/main/java/se/codelabs/timereport/api/controller/EventController.java
+++ src/main/java/se/codelabs/timereport/api/controller/EventController.java
@@ -1,6 +1,8 @@
 package se.codelabs.timereport.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -8,6 +10,7 @@ import se.codelabs.timereport.api.model.Event;
 import se.codelabs.timereport.api.repositories.EventRepository;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 public class EventController {
@@ -21,15 +24,28 @@ public class EventController {
 
     }
 
-    @PostMapping(path = "/events", consumes = "application/json", produces = "application/json")
+    @PostMapping(path = "/events", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> createEvent(@RequestBody Event event) {
         Event createdEvent = repository.save(event);
 
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{name}/{date}")
-                .buildAndExpand(createdEvent.getUserid(), createdEvent.getDate()).toUri();
+                .buildAndExpand(createdEvent.getUserId(), createdEvent.getDate()).toUri();
 
         return ResponseEntity.created(location).build();
 
+    }
+
+    @GetMapping(value = "/user/{userid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Event>> getEventByUserId(@PathVariable("userid") String userid) {
+        List<Event> events = repository.findByUserId(userid);
+        return new ResponseEntity<List<Event>>(events, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/user/{userid}/date/{date}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Event>> getEventByUserIdAndDate(@PathVariable("userid") String userId,
+                                                             @PathVariable("date") String date) {
+        List<Event> events = repository.findByUserIdAndDate(userId, date);
+        return new ResponseEntity<List<Event>>(events, HttpStatus.OK);
     }
 
 
--- src/main/java/se/codelabs/timereport/api/repositories/EventRepository.java
+++ src/main/java/se/codelabs/timereport/api/repositories/EventRepository.java
@@ -3,13 +3,13 @@ package se.codelabs.timereport.api.repositories;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 import se.codelabs.timereport.api.model.Event;
-import se.codelabs.timereport.api.model.EventId;
 
 import java.util.List;
 
 @EnableScan
-public interface EventRepository extends CrudRepository<Event, EventId> {
+public interface EventRepository extends CrudRepository<Event, String> {
 
-    List<Event> findByEventId(EventId id);
+    List<Event> findByUserId(String userId);
+    List<Event> findByUserIdAndDate(String userId, String date);
 
 }
