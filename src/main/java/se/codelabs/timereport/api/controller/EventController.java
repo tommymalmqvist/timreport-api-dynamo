7b131d51fc8915508a9127ecf2d5931f8d61e9b7
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2019-03-23 19:00:00 +0100

    Use ResponseEntity for createEvent

--- src/main/java/se/codelabs/timereport/api/controller/EventController.java
+++ src/main/java/se/codelabs/timereport/api/controller/EventController.java
@@ -1,10 +1,13 @@
 package se.codelabs.timereport.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import se.codelabs.timereport.api.model.Event;
 import se.codelabs.timereport.api.repositories.EventRepository;
 
+import java.net.URI;
 
 @RestController
 public class EventController {
@@ -19,8 +22,14 @@ public class EventController {
     }
 
     @PostMapping(path = "/events", consumes = "application/json", produces = "application/json")
-    public void saveEvent(@RequestBody Event event) {
-        repository.save(event);
+    public ResponseEntity<Object> createEvent(@RequestBody Event event) {
+        Event createdEvent = repository.save(event);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{name}/{date}")
+                .buildAndExpand(createdEvent.getUserid(), createdEvent.getDate()).toUri();
+
+        return ResponseEntity.created(location).build();
+
     }
 
 
